10b369934b5c6c395e8ac4267431206b0f14e9de
Billy Arnold <billyarnoldd@gmail.com>
2024-05-29 19:48:42 +0100

    board setup and starting tick

--- tdd-mooc-tetris-main/src/Board.mjs
+++ tdd-mooc-tetris-main/src/Board.mjs
@@ -5,9 +5,33 @@ export class Board {
   constructor(width, height) {
     this.width = width;
     this.height = height;
+
+    this.cells = [];
+
+    for (let row = 0; row < height; row++) {
+      this.cells.push([]);
+      for (let column = 0; column < width; column++) {
+        this.cells[row].push(".");
+      }
+    }
   }
 
   toString() {
-    return "TODO";
+    return this.cells.map((row) => row.join("")).join("\n") + "\n";
+  }
+
+  drop() {
+    this.cells[0][1] = "X";
+  }
+
+  tick() {
+    for (let row = 0; row < this.height; row++) {
+      for (let column = 0; column < this.width; column++) {
+        if (this.cells[row][column] === "X") {
+          this.cells[row][column] = ".";
+          this.cells[row + 1][column] = "X";
+        }
+      }
+    }
   }
 }

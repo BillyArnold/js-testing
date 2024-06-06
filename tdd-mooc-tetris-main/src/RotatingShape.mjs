65b3fae3c0a20eec2b7705294b52d21b6b9df8f9
Billy Arnold <billyarnoldd@gmail.com>
2024-06-06 08:42:28 +0100

    setting up rotating shape

--- tdd-mooc-tetris-main/src/RotatingShape.mjs
+++ tdd-mooc-tetris-main/src/RotatingShape.mjs
@@ -1,1 +1,13 @@
-export default class RotatingShape {}
+export default class RotatingShape {
+  constructor(shape) {
+    this.shape = shape;
+  }
+
+  fromString(string) {
+    return new RotatingShape(string);
+  }
+
+  toString() {
+    return this.shape;
+  }
+}
